0f29e4ccbb4a0cf6bc203c6bcef557756b2862ce
Marcos <52284432+torecsgo@users.noreply.github.com>
2024-08-22 16:11:39 +0200

    ACTIVATE/DESACTIVATE SOCD/SnapTap
    Activate and Desactivate with F13, in Layout (fn1 + backspace)

--- keymaps/snaptap/keymap.c
+++ keymaps/snaptap/keymap.c
@@ -12,9 +12,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * SnapTap Modifications By TORE
  */
 
 #include QMK_KEYBOARD_H
+
+bool snaptap_activated = false; // Estado del SnapTap
 
 // Variables globales para manejar el estado de las teclas A y D
 bool a_held = false;    // Estado real de la tecla 'A'
@@ -68,113 +73,131 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______),
 
     [_FN3] = LAYOUT_60_iso(
-        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,
+        KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   KC_F13,
         RGB_TOG, RGB_MOD,  RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______,  _______,  _______,
         _______, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______,  _______,  _______,  _______,
         _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______,            _______,
         _______, _______,  _______,                            _______,                            _______,  _______,  _______,  _______)
 };
 
-// Lógica para manejar el comportamiento de las teclas 'A' y 'D'
+// Lógica para manejar el comportamiento de las teclas 'A'/'D' y 'W'/'S'
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_A:
-            if (record->event.pressed) {
-                a_held = true;
-                if (d_scrip) {
-                    d_scrip = false;
-                    unregister_code(KC_D);  // Simula la liberación de la tecla 'D'
-                }
-                if (!a_scrip) {
-                    a_scrip = true;
-                    register_code(KC_A);   // Simula la pulsación de la tecla 'A'
-                }
-            } else {
-                a_held = false;
-                if (a_scrip) {
-                    a_scrip = false;
-                    unregister_code(KC_A); // Simula la liberación de la tecla 'A'
-                }
-                if (d_held && !d_scrip) {
-                    d_scrip = true;
-                    register_code(KC_D);   // Simula la pulsación de la tecla 'D'
-                }
-            }
-            return false; // Evita la acción predeterminada de 'A'
-        
-        case KC_D:
-            if (record->event.pressed) {
-                d_held = true;
-                if (a_scrip) {
-                    a_scrip = false;
-                    unregister_code(KC_A); // Simula la liberación de la tecla 'A'
-                }
-                if (!d_scrip) {
-                    d_scrip = true;
-                    register_code(KC_D);   // Simula la pulsación de la tecla 'D'
-                }
-            } else {
-                d_held = false;
-                if (d_scrip) {
-                    d_scrip = false;
-                    unregister_code(KC_D); // Simula la liberación de la tecla 'D'
-                }
-                if (a_held && !a_scrip) {
-                    a_scrip = true;
-                    register_code(KC_A);   // Simula la pulsación de la tecla 'A'
-                }
-            }
-            return false; // Evita la acción predeterminada de 'D'
+	if (snaptap_activated) {
+		switch (keycode) {
+			case KC_A:
+				if (record->event.pressed) {
+					a_held = true;
+					if (d_scrip) {
+						d_scrip = false;
+						unregister_code(KC_D);  // Simula la liberación de la tecla 'D'
+					}
+					if (!a_scrip) {
+						a_scrip = true;
+						register_code(KC_A);   // Simula la pulsación de la tecla 'A'
+					}
+				} else {
+					a_held = false;
+					if (a_scrip) {
+						a_scrip = false;
+						unregister_code(KC_A); // Simula la liberación de la tecla 'A'
+					}
+					if (d_held && !d_scrip) {
+						d_scrip = true;
+						register_code(KC_D);   // Simula la pulsación de la tecla 'D'
+					}
+				}
+				return false; // Evita la acción predeterminada de 'A'
 			
-		case KC_W:
-            if (record->event.pressed) {
-                w_held = true;
-                if (s_scrip) {
-                    s_scrip = false;
-                    unregister_code(KC_S);  // Simula la liberación de la tecla 'S'
-                }
-                if (!w_scrip) {
-                    w_scrip = true;
-                    register_code(KC_W);   // Simula la pulsación de la tecla 'W'
-                }
-            } else {
-                w_held = false;
-                if (w_scrip) {
-                    w_scrip = false;
-                    unregister_code(KC_W); // Simula la liberación de la tecla 'W'
-                }
-                if (s_held && !s_scrip) {
-                    s_scrip = true;
-                    register_code(KC_S);   // Simula la pulsación de la tecla 'S'
-                }
-            }
-            return false; // Evita la acción predeterminada de 'W'
-        
-        case KC_S:
-            if (record->event.pressed) {
-                s_held = true;
-                if (w_scrip) {
-                    w_scrip = false;
-                    unregister_code(KC_W); // Simula la liberación de la tecla 'W'
-                }
-                if (!s_scrip) {
-                    s_scrip = true;
-                    register_code(KC_S);   // Simula la pulsación de la tecla 'S'
-                }
-            } else {
-                s_held = false;
-                if (s_scrip) {
-                    s_scrip = false;
-                    unregister_code(KC_S); // Simula la liberación de la tecla 'S'
-                }
-                if (w_held && !w_scrip) {
-                    w_scrip = true;
-                    register_code(KC_W);   // Simula la pulsación de la tecla 'W'
-                }
-            }
-            return false; // Evita la acción predeterminada de 'S'
+			case KC_D:
+				if (record->event.pressed) {
+					d_held = true;
+					if (a_scrip) {
+						a_scrip = false;
+						unregister_code(KC_A); // Simula la liberación de la tecla 'A'
+					}
+					if (!d_scrip) {
+						d_scrip = true;
+						register_code(KC_D);   // Simula la pulsación de la tecla 'D'
+					}
+				} else {
+					d_held = false;
+					if (d_scrip) {
+						d_scrip = false;
+						unregister_code(KC_D); // Simula la liberación de la tecla 'D'
+					}
+					if (a_held && !a_scrip) {
+						a_scrip = true;
+						register_code(KC_A);   // Simula la pulsación de la tecla 'A'
+					}
+				}
+				return false; // Evita la acción predeterminada de 'D'
+				
+			case KC_W:
+				if (record->event.pressed) {
+					w_held = true;
+					if (s_scrip) {
+						s_scrip = false;
+						unregister_code(KC_S);  // Simula la liberación de la tecla 'S'
+					}
+					if (!w_scrip) {
+						w_scrip = true;
+						register_code(KC_W);   // Simula la pulsación de la tecla 'W'
+					}
+				} else {
+					w_held = false;
+					if (w_scrip) {
+						w_scrip = false;
+						unregister_code(KC_W); // Simula la liberación de la tecla 'W'
+					}
+					if (s_held && !s_scrip) {
+						s_scrip = true;
+						register_code(KC_S);   // Simula la pulsación de la tecla 'S'
+					}
+				}
+				return false; // Evita la acción predeterminada de 'W'
 
-        default:
-            return true;  // Procesa todas las otras teclas normalmente
-    }
+			case KC_S:
+				if (record->event.pressed) {
+					s_held = true;
+					if (w_scrip) {
+						w_scrip = false;
+						unregister_code(KC_W); // Simula la liberación de la tecla 'W'
+					}
+					if (!s_scrip) {
+						s_scrip = true;
+						register_code(KC_S);   // Simula la pulsación de la tecla 'S'
+					}
+				} else {
+					s_held = false;
+					if (s_scrip) {
+						s_scrip = false;
+						unregister_code(KC_S); // Simula la liberación de la tecla 'S'
+					}
+					if (w_held && !w_scrip) {
+						w_scrip = true;
+						register_code(KC_W);   // Simula la pulsación de la tecla 'W'
+					}
+				}
+				return false; // Evita la acción predeterminada de 'S'
+				
+			case KC_F13:
+				if (record->event.pressed) {
+					snaptap_activated = false; // Modifica el estado del SnapTap
+				}
+				return false;
+				
+			default:
+				return true;  // Procesa todas las otras teclas normalmente
+		}
+	} else {
+		switch (keycode) {
+			case KC_F13:
+				if (record->event.pressed) {
+					snaptap_activated = true; // Modifica el estado del SnapTap
+				}
+				return false;
+			default:
+				return true;  // Procesa todas las otras teclas normalmente
+		}
+	}
 }
